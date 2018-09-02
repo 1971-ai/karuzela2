c5c42c97fd7f3ff0d827068db853bd5e2082488f
Artur Idzikowski <aaidzikowscy@outlook.com>
2018-09-03 01:34:38 +0200

    Add new files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+'use strict';
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true
+
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
+
+});
+var flkty1 = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty1.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
