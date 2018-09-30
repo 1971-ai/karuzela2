6ff4a75b509c2e27eb457ae897eeb87a5e07a541
Artur Idzikowski <aaidzikowscy@outlook.com>
2018-09-30 10:14:16 +0200

    Update files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 'use strict';
+var templateCarousel = document.getElementById('template-carousel').innerHTML;
+Mustache.parse(templateCarousel);
+var listItems = '';
+var results = document.querySelector('.main-carousel');
+for(var i = 0; i < carouselData.length; i++){
+	listItems += Mustache.render(templateCarousel, carouselData[i]);
+}
+results.insertAdjacentHTML('beforeend', listItems);
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   cellAlign: 'left',
@@ -10,6 +18,13 @@ var restart = document.getElementById("restart");
 restart.addEventListener('click', function(){
 	flkty.select(0)
 });
+var carouselIndex = document.querySelectorAll("#carousel-index a");
+for(var i = 0; i < carouselIndex.length; i++){
+	carouselIndex[i].addEventListener('click', function(e){
+		e.preventDefault();
+    	flkty.select(+carouselIndex[i].getAttribute('data-index'));
+	});
+}
 
 var progressBar = document.querySelector('.progress-bar')
 
